d113a8041fe54aaa7f826d0a2bd6c2f93d4d85c8
TheUserOne <theuserone@yandex.ru>
2023-08-15 22:05:40 +0700

    beginning

---
+++ main.py
@@ -0,0 +1,76 @@
+# 1. The sum of the digits of a three-digit number
+
+# print ('Enter an integer')
+# abc = int(input())
+# c = abc % 10
+# ab = abc // 10
+# b = ab % 10
+# a = ab // 10
+# print(a + b + c)
+
+# print ('Enter an integer')
+# n = int(input())
+# m = int(10)
+# a = int (0)
+# sum = int (0)
+
+# while n // 10 > 0:
+#     a = n % m
+#     n = n // m
+#     sum = sum + a
+    
+# sum = sum + n
+# print(sum)
+
+
+# 2.
+# Сколько сделал каждый журавликов, если
+# Петя, Катя, Сережа делают журавликов из бумаги.
+# Вместе они сделали S журавликов.
+# Петя сделал = Сережа сделал.
+# Катя сделала сделал в два раза больше, чем (Петя + Сережа)
+
+# print ('Enter how many cranes were made by kids')
+# s = int(input())
+# a = s / 6
+# print (f"Peter = {a}, Serg = {a}, Kate = {4 * a}")
+
+
+
+# 3. Шестизначное число
+# Проверить равность сум первых трёх и цифр и последних трёх цифр
+
+# print ('Enter an six-digit integer')
+# n = int(input())
+# m = int(10)
+# sumA = int(0)
+# count = int(0)
+
+# while count < 3:
+#     a = n % m
+#     n = n // m
+#     sumA= sumA + a
+#     count += 1
+
+# print (sumA)
+# count = 0
+# sumB = int(0)
+
+# while count < 3:
+#     a = n % m
+#     n = n // m
+#     sumB = sumB + a
+#     count += 1
+
+# print (sumB)
+
+# if sumA == sumB:
+#     print("Congratulations")
+# else:
+#     print("I'm sorry Better Luck Next Time")
+
+
+
+# 4. Chokolate = n * m
+# Chokolate / n = x * m ?
+# Chokolate / m = x * n ?
